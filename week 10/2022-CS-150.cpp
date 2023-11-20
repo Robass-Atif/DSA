c005382bf8023a1229096a8f5ba41f458ccef702
robass atif <mohammadrobass@gmail.com>
2023-11-20 20:15:01 +0500

    msg

--- week 10/2022-CS-150.cpp
+++ week 10/2022-CS-150.cpp
@@ -356,3 +356,71 @@ int main()
 //     }
 // };
 
+//  problem 3
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// class Solution {
+// public:
+//     vector<int> postorderTraversal(TreeNode* root) {
+//         vector<int> result;
+//         Post(root,result);
+//         return result;
+        
+//     }
+    
+//     void Post(TreeNode* root,vector<int> &result )
+//     {
+//         if(root==NULL)
+//         {
+//             return;
+//         }
+//         Post(root->left,result);
+//         Post(root->right,result);
+//         result.push_back(root->val);
+//     }
+// };
+
+// problem 3
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// class Solution {
+// public:
+//     vector<int> preorderTraversal(TreeNode* root) {
+        
+//         vector<int> result;
+//         pre(root,result);
+//         return result;
+        
+//     }
+//     void pre(TreeNode* root,vector<int> &result)
+//     {
+//         if(!root)
+//         {
+//             return;
+//         }
+//      result.push_back(root->val);
+
+//         pre(root->left,result);
+        
+//         pre(root->right,result);
+        
+//     }
+// };
